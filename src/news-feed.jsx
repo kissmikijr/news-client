1276f0762919125238201ad002ffa12613281682
Miklos Kiss <kissmiklosjr@gmail.com>
2020-04-24 16:44:02 +0200

    add scrollto on keypress

--- src/news-feed.jsx
+++ src/news-feed.jsx
@@ -1,57 +1,56 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import NewsCard from "./news-card";
 
 const scrollToRef = (ref) => {
-  console.log(ref);
-  window.scrollTo(0, ref.current.offsetTop);
+  if (!ref || !ref.current) return;
+  window.scrollTo({ left: 0, top: ref.current.offsetTop, behavior: "smooth" });
 };
 
 const NewsFeed = () => {
   const [feedData, setFeedData] = useState([]);
-  const [currentRef, setCurrentRef] = useState(0);
+  const [refs, setRefs] = useState([]);
+  let currentRef = 0;
 
   useEffect(() => {
-    window.addEventListener("keypress", keyPress);
     const getHeadlines = async () => {
       const result = await axios.get(
         "https://neeews.herokuapp.com/api/news/headlines"
       );
+
+      const refs = result.data.data.map(() => React.createRef());
+      const keyPressHandler = (event) => {
+        if (event.keyCode === 106 || event.charCode === 106) {
+          scrollToRef(refs[currentRef + 1]);
+          currentRef++;
+        } else if (event.keyCode === 107 || event.charCode === 107) {
+          if (currentRef === 0) return;
+          scrollToRef(refs[currentRef - 1]);
+          currentRef--;
+        }
+      };
+
+      window.addEventListener("keypress", keyPressHandler);
+      setRefs(refs);
       setFeedData(result.data.data);
+
+      return () => {
+        window.removeEventListener("keypress", keyPressHandler);
+      };
     };
     getHeadlines();
+  }, [currentRef]);
 
-    return () => {
-      window.removeEventListener("keypress", keyPress);
-    };
-  }, []);
-
-  const refs = feedData.map((x) => React.createRef());
-  console.log(refs, "@@");
-
-  const keyPress = (event) => {
-    console.log(refs);
-    if (event.keyCode === 106 || event.charCode === 106) {
-      scrollToRef(refs[currentRef - 1]);
-      setCurrentRef(currentRef - 1);
-    } else if (event.keyCode === 107 || event.charCode === 107) {
-      scrollToRef(refs[currentRef + 1]);
-      setCurrentRef(currentRef + 1);
-    }
-  };
-  const renderFeed = () => {
-    const result = [];
-    for (let i = 0; i < feedData.length; i++) {
-      const news = feedData[i];
-      result.push(
-        <div ref={refs[i]}>
-          <NewsCard news={news} key={i} />
-        </div>
-      );
-    }
-    return result;
-  };
-  return <>{renderFeed()}</>;
+  const result = [];
+  for (let i = 0; i < feedData.length; i++) {
+    const news = feedData[i];
+    result.push(
+      <div ref={refs[i]}>
+        <NewsCard news={news} key={i} />
+      </div>
+    );
+  }
+  return result;
 };
 export default NewsFeed;
