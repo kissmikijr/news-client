eacc4269d7a6bdf30f61a4762e3b16d47ce1c795
Miklos Kiss <kissmiklosjr@gmail.com>
2020-04-24 02:43:18 +0200

    trying to add scrolltoview feature

--- src/news-feed.jsx
+++ src/news-feed.jsx
@@ -1,11 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import NewsCard from "./news-card";
 
+const scrollToRef = (ref) => {
+  console.log(ref);
+  window.scrollTo(0, ref.current.offsetTop);
+};
+
 const NewsFeed = () => {
   const [feedData, setFeedData] = useState([]);
+  const [currentRef, setCurrentRef] = useState(0);
+
   useEffect(() => {
+    window.addEventListener("keypress", keyPress);
     const getHeadlines = async () => {
       const result = await axios.get(
         "https://neeews.herokuapp.com/api/news/headlines"
@@ -13,14 +21,37 @@ const NewsFeed = () => {
       setFeedData(result.data.data);
     };
     getHeadlines();
+
+    return () => {
+      window.removeEventListener("keypress", keyPress);
+    };
   }, []);
-  console.log(feedData);
-  return (
-    <>
-      {feedData.map((news) => (
-        <NewsCard news={news} key={news.publishedAt} />
-      ))}
-    </>
-  );
+
+  const refs = feedData.map((x) => React.createRef());
+  console.log(refs, "@@");
+
+  const keyPress = (event) => {
+    console.log(refs);
+    if (event.keyCode === 106 || event.charCode === 106) {
+      scrollToRef(refs[currentRef - 1]);
+      setCurrentRef(currentRef - 1);
+    } else if (event.keyCode === 107 || event.charCode === 107) {
+      scrollToRef(refs[currentRef + 1]);
+      setCurrentRef(currentRef + 1);
+    }
+  };
+  const renderFeed = () => {
+    const result = [];
+    for (let i = 0; i < feedData.length; i++) {
+      const news = feedData[i];
+      result.push(
+        <div ref={refs[i]}>
+          <NewsCard news={news} key={i} />
+        </div>
+      );
+    }
+    return result;
+  };
+  return <>{renderFeed()}</>;
 };
 export default NewsFeed;
